dd0bacbecd31b5c44c201d97987f1eaf6d496816
Stephen Welch <stephen@Stephens-MBP.attlocal.net>
2025-02-27 09:27:38 -0500

    this last big transition to absorbed MLA is no joke

--- _2025/deepseek/p34_42.py
+++ _2025/deepseek/p34_42.py
@@ -97,9 +97,76 @@ def get_niave_mla_head(layer_id=0):
 
         return Group(all_images, all_labels)
 
+def get_mla_head(layer_id=0):
+    img_path=Path('/Users/stephen/welch_labs/deepseek/hackin/linux_workdir/deepseek/deepseek_viz_absorbed_1')
+    svg_path=Path('/Users/stephen/welch_labs/deepseek/graphics/to_manim')
+    svg_file='mla_panels-'
 
+    # self.frame.reorient(0, 86, 0, (-0.07, -0.06, 0.06), 2.39)
+    # self.frame.reorient(0, 0, 0, (0.58, 0.21, 0.0), 1.86)
 
-class p34_38(InteractiveScene):
+    svg_files=list(sorted((svg_path).glob('*'+svg_file+'*')))
+    # print(svg_files)
+
+    all_labels=Group()
+    for svg_file in svg_files[1:]: #Skip first artboard
+        svg_image=SVGMobject(str(svg_file))
+        all_labels.add(svg_image[1:]) #Thowout background
+
+    # self.add(all_labels[4:7])
+    # self.add(all_labels[8:])
+
+    # layer_id=0
+
+    q1=ImageMobject(str(img_path/str(layer_id)/'q_latent.png'))
+    q1.scale([0.0075, 0.14, 1]) 
+    q1.move_to([-0.285,0.6,0]) 
+    # self.add(q1)
+
+    # self.remove(q1)      
+
+    k1=ImageMobject(str(img_path/'kv2.png'))
+    k1.scale([0.0075, 0.14, 1]) 
+    k1.move_to([-0.285,-0.01,0]) 
+    # self.add(k1)
+
+    # self.remove(k1)
+
+    kt=ImageMobject(str(img_path/'kv2.png'))
+    kt.scale([0.004, 0.045, 1])
+    kt.rotate([0, 0, -PI/2]) 
+    kt.move_to([0.293,0.485,0])     
+    # self.add(kt)
+# 
+    # self.remove(kt)
+
+    a1=ImageMobject(str(img_path/str(layer_id)/'scores_single_1.png'))
+    a1.scale([0.055,0.055, 1])
+    a1.move_to([0.56,0.56,0]) 
+    # self.add(a1)
+
+    # self.remove(a1)
+
+    a2=ImageMobject(str(img_path/str(layer_id)/'scores_single_4.png'))
+    a2.scale([0.13,0.13, 1])
+    a2.move_to([1.155,0.445,0]) 
+    # self.add(a2)
+
+    # self.remove(a2)
+
+    z1=ImageMobject(str(img_path/str(layer_id)/'alkv.png'))
+    z1.scale([0.0079, 0.10, 1]) 
+    z1.move_to([0.91,-0.2,0]) 
+    # self.add(z1)
+
+    # self.remove(z1)
+
+    all_images=Group(q1, k1, kt, a1, a2, z1)
+    # self.embed()
+
+    return Group(all_images, all_labels)
+
+class p34_42(InteractiveScene):
     def construct(self):
         '''This sequence and the next will be a bit tricky, but I'll get some good mileage out of them 
            and then be home free. With this first one, I do like the idea of startiwth full 3d Niave MLA,
@@ -162,16 +229,63 @@ class p34_38(InteractiveScene):
         self.play(*[FadeOut(attention_heads[i][1][4:6]) for i in range(12)]+
                    [FadeOut(connector_1)]+
                    [attention_heads[0][0][2].animate.move_to(og_kv_cache_center)]+
-                   [self.frame.animate.reorient(0, 90, 0, (-0.05, 0.79, -0.04), 2.70)], #Placeholder position will need to tweak
+                   [self.frame.animate.reorient(0, 90, 0, (-0.05, 0.79, -0.04), 2.70)], 
                    run_time=4) #Do an option with cranked up runtime - this covers a couple paragraphs
         
-        for i in range(1, 12): self.remove(attention_heads[i])
+        for i in range(1, 12): 
+            self.remove(attention_heads[i])
         self.add(attention_heads[0][0][:2])
         self.add(attention_heads[0][1][:2])
 
         self.wait()
 
+        # Considering zooming in here - but there's a lot going on visually and I think everything is visible - I think 
+        # we pretty much hold this view and highlight stuff in editing for p37 and p38. 
+        # Eh stuff is kinda small, let me add a little zoom in/zoom out - I can edit this out if I don't like it. 
+
+        self.play(attention_heads[0][0][3].animate.set_opacity(0.1), attention_heads[0][0][5].animate.set_opacity(0.1), 
+                  attention_heads[0][0][7:].animate.set_opacity(0.1), 
+                  attention_heads[0][1][8].animate.set_opacity(0.1),
+                  attention_heads[0][1][11:17].animate.set_opacity(0.1),
+                  self.frame.animate.reorient(0, 89, 0, (-0.69, 0.79, -0.26), 2.18), run_time=2) #Zoom in to new steps
+
+        self.wait()
+
+
+        self.play(attention_heads[0][0][3].animate.set_opacity(1.0), attention_heads[0][0][5].animate.set_opacity(1.0), 
+                  attention_heads[0][0][7:].animate.set_opacity(1.0), 
+                  attention_heads[0][1][8].animate.set_opacity(1.0),
+                  attention_heads[0][1][11:17].animate.set_opacity(1.0),
+                  self.frame.animate.reorient(0, 90, 0, (-0.05, 0.79, -0.04), 2.70), run_time=2) #Zoom back out
+        self.wait()
+
+
         # self.frame.reorient(0, 89, 0, (-0.05, 0.79, -0.04), 2.67)
+
+        #Ok so I was going to do a break here to a new class and go back to 2d -> but since I want to go back to 3d at 
+        # the end, perhaps i do the "absorbption" in 3d space - this is a little scary but I think i can figure it out
+        # and it will pay off in being able to pop right back to 3d. 
+        # Ok, now how do I do the matrix absorption?
+        # Maybe I get the end state together here and then figure out how to animate to it?
+        # Man kinda a lot changes! I don't want it to necessarily feel like that though - ya know?
+        mla_heads=Group()
+        spacing=0.25
+        for i in range(12): 
+            a=get_mla_head(layer_id=6*i)
+            a.rotate([PI/2,0,0], axis=RIGHT)
+            a.move_to([0, spacing*i,0]) #Probably some shifting and scaling I need to come back and apply here
+            mla_heads.add(a)
+
+
+        self.wait()
+        # Hmm do i actually just want the names of the matrices to move? And the arrows and boxes just fade in/out?
+        # That might be cleaner
+        self.add(mla_heads[0][1][1].move_to([-0.,  0, 0.45]))
+
+
+        self.remove(mla_heads[0][1][1])
+
+
 
 
         self.wait(20)

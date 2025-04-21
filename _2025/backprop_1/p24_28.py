025b9c830c7958eba63d4269b1ba75e4a5130dfe
Stephen Welch <stephen@Stephens-MBP.attlocal.net>
2025-04-20 20:49:15 -0400

    ok picking up in the am

--- _2025/backprop_1/p24_28.py
+++ _2025/backprop_1/p24_28.py
@@ -25,5 +25,15 @@ class P24v1(InteractiveScene):
         )
 
 
-        self.add(axes)
-        
+
+        # Add labels
+        x_label = Tex(r'\theta_{1}', font_size=28).set_color(CHILL_BROWN)
+        y_label = Tex(r'\theta_{2}', font_size=28).set_color(CHILL_BROWN)
+        z_label = Tex('Loss', font_size=28).set_color(CHILL_BROWN)
+        x_label.next_to(axes.x_axis, RIGHT)
+        y_label.next_to(axes.y_axis, UP)
+        z_label.next_to(axes.z_axis, OUT)
+        z_label.rotate(90*DEGREES, [1,0,0])
+
+
+        self.add(axes, x_label, y_label, z_label)

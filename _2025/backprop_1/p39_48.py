7358f6443e5d2f214455cf08a8cc73ecfc4afb56
Stephen Welch <stephen@Stephens-MBP.attlocal.net>
2025-04-26 06:42:33 -0400

    fixed rendering bug and am hacking on 48 now

--- _2025/backprop_1/p39_48.py
+++ _2025/backprop_1/p39_48.py
@@ -119,7 +119,7 @@ def manual_camera_interpolation(start_orientation, end_orientation, num_steps):
     return result
 
 
-class P39_48(InteractiveScene):
+class P39_47(InteractiveScene):
     def construct(self):
         '''
         Ok this time I'm goint to start with setting up the surface and gridlines, at the origin. 
@@ -680,11 +680,6 @@ class P39_48(InteractiveScene):
         # Really closer here -> need to include "dot falling" with my animation - and get rid of otehr black dot. 
 
 
-
-
-
-
-
         ## ----------------------------------------------------------------------- ##
         # self.frame.reorient(88, 41, 0, (-0.71, -0.37, 0.21), 2.60)
         # reorient(91, 28, 0, (-0.49, -0.34, 0.08), 2.60)
@@ -702,9 +697,6 @@ class P39_48(InteractiveScene):
         #     dot_path.add(Dot3D(center=t, radius=0.017, color='$FF00FF'))
         # self.add(dot_path)
 
-
-
-        
 
         
         # ts.set_opacity(1.0)
@@ -741,8 +733,6 @@ class P39_48(InteractiveScene):
 
         # self.frame.reorient(43, 51, 0, (0.07, 0.08, 0.01), 6.29)
 
-        
-
         #Scale and move axes in a similiar way, but move them up more. 
 
 
@@ -760,23 +750,117 @@ class P39_48(InteractiveScene):
         # self.add(u_gridlines, v_gridlines)
 
 
-
-
         self.embed()
         self.wait(20)
 
 
+class P48_moving_view_1(InteractiveScene):
+    def construct(self):
+        
+        starting_coords=[0.05,-0.9]
+        starting_point=param_surface_1(*starting_coords)
+        s2=Dot3D(center=starting_point, radius=0.06, color='$FF00FF')
+    
+        #Load up other surfaces to visualize
+        loss_arrays=[]
+        num_time_steps=5
+        for i in range(num_time_steps):
+            loss_arrays.append(np.load('/Users/stephen/Stephencwelch Dropbox/Stephen Welch/welch_labs/backpropagation/hackin/apr_25_1/'+str(i).zfill(3)+'.npy'))
+
+        # import matplotlib.pyplot as plt
+        # for i in range(num_time_steps):
+        #     plt.clf()
+        #     plt.figure(frameon=False)
+        #     ax = plt.Axes(plt.gcf(), [0., 0., 1., 1.])
+        #     ax.set_axis_off()
+        #     plt.gcf().add_axes(ax)
+        #     plt.imshow(np.rot90(loss_arrays[i].T)) #have to transpose if transposing u and v and param_surface_1
+        #     plt.savefig('loss_2d_1_'+str(i).zfill(3)+'.png', bbox_inches='tight', pad_inches=0, dpi=300)
+        #     plt.close()
+
+
+        surfaces=Group()
+        surf_functions=[] #Need this later to move dot around.
+        grids=Group()
+        for i in range(num_time_steps):
+            surf_func=partial(param_surface_2, surf_array=loss_arrays[i])
+            surf_functions.append(surf_func)
+            surface = ParametricSurface(
+                surf_func,  
+                u_range=[-2.5, 2.5],
+                v_range=[-2.5, 2.5],
+                resolution=(512, 512),
+            )
+
+            ts2 = TexturedSurface(surface, '/Users/stephen/manim/videos/'+'loss_2d_1_'+str(i).zfill(3)+'.png')
+            ts2.set_shading(0.0, 0.1, 0)
+            surfaces.add(ts2)
+
+            num_lines = 64  # Number of gridlines in each direction
+            num_points = 512  # Number of points per line
+            u_gridlines = VGroup()
+            v_gridlines = VGroup()
+            u_values = np.linspace(-2.5, 2.5, num_lines)
+            v_points = np.linspace(-2.5, 2.5, num_points)
+            for u in u_values:
+                points = [param_surface_1(u, v) for v in v_points]
+                line = VMobject()
+                line.set_points_smoothly(points)
+                line.set_stroke(width=1, color=WHITE, opacity=0.15)
+                u_gridlines.add(line)
+
+            u_points = np.linspace(-2.5, 2.5, num_points)
+            for v in u_values:  # Using same number of lines for both directions
+                points = [param_surface_1(u, v) for u in u_points]
+                line = VMobject()
+                line.set_points_smoothly(points)
+                line.set_stroke(width=1, color=WHITE, opacity=0.15)
+                v_gridlines.add(line)
+            grids.add(VGroup(u_gridlines, v_gridlines))
+
+        self.wait()
+
+
+        self.add(surfaces[0])
+        self.add(grids[0])
+        self.add(s2)
+
+
+        num_total_steps=16 #Crank this for final viz
+        start_orientation=[142, 34, 0, (-0.09, -0.77, 0.15), 3.55]
+        end_orientation=[131, 31, 0, (-0.12, -0.88, 0.22), 2.90]
+        interp_orientations=manual_camera_interpolation(start_orientation, end_orientation, num_steps=num_total_steps)
+
+        surface_update_counter=1
+        frames_per_surface_upddate=np.floor(num_total_steps/num_time_steps)
+        for i in range(1, num_total_steps):
+            # print(i, len(interp_orientations))
+            if i%frames_per_surface_upddate==0 and surface_update_counter<len(surfaces):
+
+                self.remove(surfaces[surface_update_counter-1])
+                self.remove(grids[surface_update_counter-1])
+                self.add(surfaces[surface_update_counter])
+                self.add(grids[surface_update_counter])
+
+                new_point_coords=surf_functions[surface_update_counter](*starting_coords)
+                s2.move_to(new_point_coords) #This should make point move down smoothly. 
+                surface_update_counter+=1
+
+            # print(i, len(interp_orientations))
+            self.frame.reorient(*interp_orientations[i])
+            self.wait(0.1)
+
+        self.wait()
 
 
 
+class P48_fixed_view(InteractiveScene):
+    def construct(self):
+        pass
 
-
-
-
-
-
-
-
+class P48_moving_view_2(InteractiveScene):
+    def construct(self):
+        pass
 
 
 

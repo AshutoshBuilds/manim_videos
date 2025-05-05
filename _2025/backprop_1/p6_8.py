2a0d77ab444078fd2f5a14386c7ada186b28353c
Stephen Welch <stephen@Stephens-MBP.attlocal.net>
2025-05-05 05:28:57 -0400

    ready to try in edit

--- _2025/backprop_1/p6_8.py
+++ _2025/backprop_1/p6_8.py
@@ -23,25 +23,38 @@ class P6_8(InteractiveScene):
         self.add(img_2)
 
         self.frame.reorient(0, 0, 0, (-1.73, 0.07, 0.0), 2.35)
+
+        r1=RoundedRectangle(corner_radius=0.09, height=9.0/4.9, width=16.0/4.9)
+        r1.set_stroke(color=CHILL_BROWN, width=6.0)
+        r1.move_to([-1.735, -0.015, 0])     
         
-        self.play(ShowCreation(v[24])) #Left border
+        self.play(ShowCreation(r1), run_time=2)
+        self.play(FadeIn(v[2:14])) #Part 1 label
         self.wait()
 
-        self.play(ShowCreation(v[2:14]))
+        img_1.set_opacity(1.0)
+        self.wait()
 
-        self.add(v[2:14]) #Part 1 label
+
         self.add(v[14:24]) #Part 2 label
 
-        r1=RoundedRectangle(corner_radius=0.05, height=9.0/4.9, width=16.0/4.9)
-        r1.set_stroke(color=BLUE) #CHILL_BROWN)
-        r1.move_to([-1.7, 0, 0])
-        self.add(r1)
-
-        #Pick up here in the morning - I think i use manim rectangle class!
-
-
+        self.play(FadeIn(v[1]),
+                  img_2.animate.set_opacity(1.0),
+                 self.frame.animate.reorient(0, 0, 0, (-0.01, -0.02, 0.0), 4.00),
+                 run_time=4.0)
         self.wait()
 
+
+        # self.play(ShowCreation(v[24])) #Left border
+        # self.wait()
+        # self.play(ShowCreation(v[2:14]))
+
+        
+        
+
+
+        # self.add(r1)
+        #Pick up here in the morning - I think i use manim rectangle class!
 
 
 

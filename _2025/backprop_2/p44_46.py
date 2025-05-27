52636741a2fcd696b96b083081957df9dcb2cfa7
Stephen Welch <stephen@Stephens-MBP.attlocal.net>
2025-05-27 12:36:17 -0400

    adam helps alot

--- _2025/backprop_2/p44_46.py
+++ _2025/backprop_2/p44_46.py
@@ -12,7 +12,7 @@ GREEN='#00a14b'
 
 svg_path='/Users/stephen/Stephencwelch Dropbox/Stephen Welch/welch_labs/backprop2/graphics/to_manim'
 data_path='/Users/stephen/Stephencwelch Dropbox/Stephen Welch/welch_labs/backprop2/hackin'
-heatmap_path='/Users/stephen/Stephencwelch Dropbox/Stephen Welch/welch_labs/backprop2/graphics/to_manim/may_27_1'
+heatmap_path='/Users/stephen/Stephencwelch Dropbox/Stephen Welch/welch_labs/backprop2/graphics/to_manim/may_27_3'
 
 # map_min_x=0.38
 # map_max_x=1.54
@@ -25,25 +25,20 @@ heatmap_path='/Users/stephen/Stephencwelch Dropbox/Stephen Welch/welch_labs/back
 # max_lat=56.0
 
 
-def format_number(num, total_chars=5):
+def format_number(num, total_chars=6, align='right'):
     """
-    Format number to always show same total character width including sign.
+    Format number to maintain consistent visual alignment for animations.
     
     Args:
         num: The number to format
-        total_chars: Total character width including decimal point and sign
+        total_chars: Total character width (should accommodate largest expected number)
+        align: 'right', 'left', or 'center' - how to align within the fixed width
     """
     abs_num = abs(num)
-    sign = '-' if num < 0 else ''
     
-    # Account for sign in character count
-    available_chars = total_chars - len(sign)
-    
-    if abs_num >= 1000:
-        # For very large numbers, use scientific notation or integer
-        formatted = f"{num:.0f}"
-    elif abs_num >= 100:
-        # 100-999: no decimal places (e.g., "123", "-123")
+    # Determine appropriate precision based on magnitude
+    if abs_num >= 100:
+        # 100+: no decimal places (e.g., "123", "-123")
         formatted = f"{num:.0f}"
     elif abs_num >= 10:
         # 10-99: one decimal place (e.g., "12.3", "-12.3")  
@@ -52,13 +47,102 @@ def format_number(num, total_chars=5):
         # 1-9: two decimal places (e.g., "1.23", "-1.23")
         formatted = f"{num:.2f}"
     else:
-        # Less than 1: adjust decimal places based on available space
-        if available_chars >= 4:  # Room for "0.xx"
-            formatted = f"{num:.2f}"
-        else:
-            formatted = f"{num:.1f}"
+        # Less than 1: two decimal places (e.g., "0.12", "-0.12")
+        formatted = f"{num:.2f}"
     
-    return formatted
+    # Pad to consistent width
+    if align == 'right':
+        return formatted.rjust(total_chars)
+    elif align == 'left':
+        return formatted.ljust(total_chars)
+    else:  # center
+        return formatted.center(total_chars)
+
+def format_number_fixed_decimal(num, decimal_places=2, total_chars=6):
+    """
+    Alternative formatter that keeps decimal point in same position.
+    Useful when you want all numbers to have the same decimal precision.
+    """
+    formatted = f"{num:.{decimal_places}f}"
+    return formatted.rjust(total_chars)
+
+def get_numbers(i, xs, weights, logits, yhats):
+    x = xs[i, -1]
+    numbers = VGroup()
+    
+    # Temperature display
+    tx = Tex(str(x) + r'^\circ')
+    tx.scale(0.13)
+    tx.move_to([-1.49, 0.02, 0])
+    numbers.add(tx)
+    
+    # Weights - using consistent formatting
+    w = weights[i, :]
+    tm1 = Tex(format_number(w[0], total_chars=6)).set_color('#00FFFF')
+    tm1.scale(0.16)
+    tm1.move_to([-1.195, 0.205, 0])
+    numbers.add(tm1)
+    
+    tm2 = Tex(format_number(w[1], total_chars=6)).set_color(YELLOW)
+    tm2.scale(0.15)
+    tm2.move_to([-1.155, 0.015, 0])
+    numbers.add(tm2)
+    
+    tm3 = Tex(format_number(w[2], total_chars=6)).set_color(GREEN)
+    tm3.scale(0.16)
+    tm3.move_to([-1.19, -0.17, 0])
+    numbers.add(tm3)
+    
+    # Biases
+    tb1 = Tex(format_number(w[3], total_chars=6)).set_color('#00FFFF')
+    tb1.scale(0.16)
+    tb1.move_to([-0.875, 0.365, 0])
+    numbers.add(tb1)
+    
+    tb2 = Tex(format_number(w[4], total_chars=6)).set_color(YELLOW)
+    tb2.scale(0.16)
+    tb2.move_to([-0.875, 0.015, 0])
+    numbers.add(tb2)
+    
+    tb3 = Tex(format_number(w[5], total_chars=6)).set_color(GREEN)
+    tb3.scale(0.16)
+    tb3.move_to([-0.88, -0.335, 0])
+    numbers.add(tb3)
+    
+    # Logits
+    tl1 = Tex(format_number(logits[i, 0], total_chars=6)).set_color('#00FFFF')
+    tl1.scale(0.16)
+    tl1.move_to([-0.52, 0.37, 0])
+    numbers.add(tl1)
+    
+    tl2 = Tex(format_number(logits[i, 1], total_chars=6)).set_color(YELLOW)
+    tl2.scale(0.16)
+    tl2.move_to([-0.52, 0.015, 0])
+    numbers.add(tl2)
+    
+    tl3 = Tex(format_number(logits[i, 2], total_chars=6)).set_color(GREEN)
+    tl3.scale(0.16)  
+    tl3.move_to([-0.52, -0.335, 0])
+    numbers.add(tl3)
+    
+    # Predictions
+    yhat1 = Tex(format_number(yhats[i, 0], total_chars=6)).set_color('#00FFFF')
+    yhat1.scale(0.16)
+    yhat1.move_to([0.22, 0.37, 0])
+    numbers.add(yhat1)
+    
+    yhat2 = Tex(format_number(yhats[i, 1], total_chars=6)).set_color(YELLOW)
+    yhat2.scale(0.16)
+    yhat2.move_to([0.22, 0.015, 0])
+    numbers.add(yhat2)
+    
+    yhat3 = Tex(format_number(yhats[i, 2], total_chars=6)).set_color(GREEN)
+    yhat3.scale(0.16)
+    yhat3.move_to([0.22, -0.335, 0])
+    numbers.add(yhat3)
+    
+    return numbers
+
 
 
 def latlong_to_canvas(lat, long, 
@@ -92,78 +176,6 @@ def latlong_to_canvas(lat, long,
     
     return x, y
 
-def get_numbers(i, xs, weights, logits, yhats):
-    x=xs[i, -1]
-
-    numbers=VGroup()
-
-
-    tx = Tex(str(x) + r'^\circ')
-    tx.scale(0.13)
-    tx.move_to([-1.49, 0.02, 0])
-    numbers.add(tx)
-
-    w=weights[i,:]
-    tm1=Tex(format_number(w[0])).set_color('#00FFFF')
-    tm1.scale(0.16)
-    tm1.move_to([-1.195, 0.205, 0])
-    numbers.add(tm1)
-
-    tm2=Tex(format_number(w[1])).set_color(YELLOW)
-    tm2.scale(0.15)
-    tm2.move_to([-1.155, 0.015, 0])
-    numbers.add(tm2)
-
-    tm3=Tex(format_number(w[2])).set_color(GREEN)
-    tm3.scale(0.16)
-    tm3.move_to([-1.19, -0.17, 0])
-    numbers.add(tm3)
-
-    tb1=Tex(format_number(w[3])).set_color('#00FFFF')
-    tb1.scale(0.16)
-    tb1.move_to([-0.875, 0.365, 0])
-    numbers.add(tb1)
-
-    tb2=Tex(format_number(w[4])).set_color(YELLOW)
-    tb2.scale(0.16)
-    tb2.move_to([-0.875, 0.015, 0])
-    numbers.add(tb2)
-
-    tb3=Tex(format_number(w[5])).set_color(GREEN)
-    tb3.scale(0.16)
-    tb3.move_to([-0.88, -0.335, 0])
-    numbers.add(tb3)
-
-    tl1=Tex(format_number(logits[i,0])).set_color('#00FFFF')
-    tl1.scale(0.16)
-    tl1.move_to([-0.52, 0.37, 0])
-    numbers.add(tl1)
-
-    tl2=Tex(format_number(logits[i,1])).set_color(YELLOW)
-    tl2.scale(0.16)
-    tl2.move_to([-0.52, 0.015, 0])
-    numbers.add(tl2)
-
-    tl3=Tex(format_number(logits[i,2])).set_color(GREEN)
-    tl3.scale(0.16)
-    tl3.move_to([-0.52, -0.335, 0])
-    numbers.add(tl3)
-
-    yhat1=Tex(format_number(yhats[i,0])).set_color('#00FFFF')
-    yhat1.scale(0.16)
-    yhat1.move_to([0.22, 0.37, 0])
-    numbers.add(yhat1)
-
-    yhat2=Tex(format_number(yhats[i,1])).set_color(YELLOW)
-    yhat2.scale(0.16)
-    yhat2.move_to([0.22, 0.015, 0])
-    numbers.add(yhat2)
-
-    yhat3=Tex(format_number(yhats[i,2])).set_color(GREEN)
-    yhat3.scale(0.16)
-    yhat3.move_to([0.22, -0.335, 0])
-    numbers.add(yhat3)
-    return numbers
 
 def get_grad_regions(i, ys, yhats, grads):
      #Ok let's shade some lines!
@@ -231,7 +243,7 @@ def get_grad_regions(i, ys, yhats, grads):
 class p44_v2(InteractiveScene):
     def construct(self):
 
-        data=np.load(data_path+'/cities_1d_1.npy')
+        data=np.load(data_path+'/cities_1d_3.npy')
         xs=data[:,:2]
         ys=data[:,2]
         weights=data[:,3:9]
@@ -522,20 +534,3 @@ class p44_v1(InteractiveScene):
 
         self.wait()
         self.embed()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

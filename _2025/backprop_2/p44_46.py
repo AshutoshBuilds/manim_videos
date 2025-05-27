1c00bc0186c832d95da1fc1116508935ab07d02f
Stephen Welch <stephen@Stephens-MBP.attlocal.net>
2025-05-26 21:14:06 -0400

    getting started on first training animation

---
+++ _2025/backprop_2/p44_46.py
@@ -0,0 +1,16 @@
+from manimlib import *
+sys.path.append('/Users/stephen/manim/videos/welch_assets')
+from welch_axes import *
+from functools import partial
+
+CHILL_BROWN='#948979'
+YELLOW='#ffd35a'
+BLUE='#65c8d0'
+
+
+class p44_v1(InteractiveScene):
+    def construct(self):
+
+        self.wait()
+        self.embed()
+

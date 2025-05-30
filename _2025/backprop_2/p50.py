046777c9d26f25082ef2259514c75ef680493699
Stephen Welch <stephen@Stephens-MBP.attlocal.net>
2025-05-30 12:47:30 -0400

    sketch seems ok

--- _2025/backprop_2/p50.py
+++ _2025/backprop_2/p50.py
@@ -12,6 +12,49 @@ GREEN='#00a14b'
 
 svg_path='/Users/stephen/Stephencwelch Dropbox/Stephen Welch/welch_labs/backprop2/graphics/to_manim'
 
+def manual_camera_interpolation(start_orientation, end_orientation, num_steps):
+    """
+    Linearly interpolate between two camera orientations.
+    
+    Parameters:
+    - start_orientation: List containing camera parameters with a tuple at index 3
+    - end_orientation: List containing camera parameters with a tuple at index 3
+    - num_steps: Number of interpolation steps (including start and end)
+    
+    Returns:
+    - List of interpolated orientations
+    """
+    result = []
+    
+    for step in range(num_steps):
+        # Calculate interpolation factor (0 to 1)
+        t = step / (num_steps - 1) if num_steps > 1 else 0
+        
+        # Create a new orientation for this step
+        interpolated = []
+        
+        for i in range(len(start_orientation)):
+            if i == 3:  # Handle the tuple at position 3
+                start_tuple = start_orientation[i]
+                end_tuple = end_orientation[i]
+                
+                # Interpolate each element of the tuple
+                interpolated_tuple = tuple(
+                    start_tuple[j] + t * (end_tuple[j] - start_tuple[j])
+                    for j in range(len(start_tuple))
+                )
+                
+                interpolated.append(interpolated_tuple)
+            else:  # Handle regular numeric values
+                start_val = start_orientation[i]
+                end_val = end_orientation[i]
+                interpolated_val = start_val + t * (end_val - start_val)
+                interpolated.append(interpolated_val)
+        
+        result.append(interpolated)
+    
+    return result
+
 class LinearPlane(Surface):
     """A plane defined by z = m1*x1 + m2*x2 + b"""
     def __init__(self, axes, m1=0.5, m2=0.3, b=1.0, vertical_viz_scale=0.5, **kwargs):
@@ -87,12 +130,69 @@ class p50_sketch(InteractiveScene):
         self.add(axes_1, plane_1, plane_2)
 
         self.frame.reorient(-29, 53, 0, (0.04, 0.06, 0.09), 2.05)
+        self.wait(0)
 
         #ok static looks good, now I want to pan around while smootly changing the LinearPlane's parameters. 
-        # start_position reorient(-33, 58, 0, (-0.1, 0.01, 0.05), 1.83)
-        # start_position reorient(33, 59, 0, (-0.1, 0.01, 0.05), 1.83)
+        num_total_steps=128
+        start_orientation = [-33, 58, 0, (-0.1, 0.01, 0.05), 1.83]
+        end_orientation = [33, 59, 0, (-0.1, 0.01, 0.05), 1.83]
+        interp_orientations=manual_camera_interpolation(start_orientation, end_orientation, num_steps=num_total_steps)
+
+        m11s=np.linspace(0.5, 0.8, num_total_steps)
+        m12s=np.linspace(1.2, 1.5, num_total_steps)
+        m21s=np.linspace(-1, -1.2, num_total_steps)
+        m22s=np.linspace(0.2, 0.5, num_total_steps)
+
+        for i in range(num_total_steps):
+
+            self.remove(plane_1, plane_2)
+
+            plane_1=LinearPlane(axes_1, m11s[i], m12s[i], 4, vertical_viz_scale=0.2)
+            plane_1.set_opacity(0.4)
+            plane_1.set_color('#00FFFF')
+
+            plane_2=LinearPlane(axes_1, m21s[i], m22s[i], 4, vertical_viz_scale=0.2)
+            plane_2.set_opacity(0.4)
+            plane_2.set_color(YELLOW)
+
+            self.add(plane_1, plane_2)
+            self.frame.reorient(*interp_orientations[i])
+
+            self.wait(0.1)
 
 
+        self.wait()
+
+
+        # num_total_steps=num_time_steps*2 #Crank this for final viz
+        # start_orientation=[142, 34, 0, (-0.09, -0.77, 0.15), 3.55]
+        # end_orientation=[121, 20, 0, (0.01, -0.46, 0.57), 1.95]
+        # interp_orientations=manual_camera_interpolation(start_orientation, end_orientation, num_steps=num_total_steps)
+
+        # surface_update_counter=1
+        # frames_per_surface_upddate=np.floor(num_total_steps/num_time_steps)
+        # for i in range(1, num_total_steps):
+        #     # print(i, len(interp_orientations))
+        #     if i%frames_per_surface_upddate==0 and surface_update_counter<len(surfaces):
+        #         if surface_update_counter==1:
+        #             self.remove(ts)
+        #             self.remove(u_gridlines, v_gridlines) 
+        #         else:
+        #             self.remove(surfaces[surface_update_counter-1])
+        #             self.remove(grids[surface_update_counter-1])
+
+        #         self.add(surfaces[surface_update_counter])
+        #         self.add(grids[surface_update_counter])
+
+        #         new_point_coords=surf_functions[surface_update_counter](*starting_coords)
+        #         s2.move_to(new_point_coords) #This should make point move down smoothly. 
+        #         surface_update_counter+=1
+
+        #     # print(i, len(interp_orientations))
+        #     self.frame.reorient(*interp_orientations[i])
+        #     self.wait(0.1)
+
+        # self.wait()
 
 
 

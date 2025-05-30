dc4ccb0196c357598ca7c19b89e7157bcfbc4467
Stephen Welch <stephen@Stephens-MBP.attlocal.net>
2025-05-30 16:27:41 -0400

    monochrome for activations?

--- _2025/backprop_2/llama_learning_animation_1.py
+++ _2025/backprop_2/llama_learning_animation_1.py
@@ -5,6 +5,8 @@ import torch
 import sys
 sys.path.append('_2025/backprop_2')
 from network_pranav_pr_1 import *
+import matplotlib.colors as mcolors
+
 
 CHILL_BROWN='#948979'
 YELLOW='#ffd35a'
@@ -24,8 +26,25 @@ def get_edge_points(circle1, circle2, neuron_radius):
     return start_point, end_point
 
 
+viridis_colormap=plt.get_cmap("viridis")
+custom_cmap_tans = mcolors.LinearSegmentedColormap.from_list('custom', ['#000000', '#dfd0b9'], N=256)
+
+# def get_nueron_color(value, vmin=-2, vmax=2):        
+#         value_clipped = np.clip((value - vmin) / (vmax - vmin), 0, 1)
+#         rgba = custom_cmap_tans(value_clipped) #Would also like to try a monochrome tan option
+#         return Color(rgb=rgba[:3])
+
+def get_nueron_color(value, vmax=2.5):        
+    '''Uses abs, a little reductive'''
+    value_clipped = np.clip(np.abs(value)/vmax, 0, 1)
+    rgba = custom_cmap_tans(value_clipped) #Would also like to try a monochrome tan option
+    return Color(rgb=rgba[:3])
+
+
 def get_mlp(w1, 
             w2,
+            neuron_fills=None, #Black if None
+            # grads,
             line_weight=1.0, 
             line_opacity=0.5, 
             neuron_stroke_width=2.0, 
@@ -55,7 +74,10 @@ def get_mlp(w1,
         else:
             neuron = Circle(radius=NEURON_RADIUS, stroke_color=neuron_stroke_color)
             neuron.set_stroke(width=neuron_stroke_width)
-            neuron.set_fill(color=BLACK, opacity=1.0)
+            if neuron_fills is None: 
+                neuron.set_fill(color='#000000', opacity=1.0)
+            else: 
+                neuron.set_fill(color=get_nueron_color(neuron_fills[0][i]), opacity=1.0)
             neuron.move_to(LEFT * LAYER_SPACING + UP * ((INPUT_NEURONS//2 - i) * VERTICAL_SPACING))
             input_layer.add(neuron)
             
@@ -68,7 +90,10 @@ def get_mlp(w1,
         else:
             neuron = Circle(radius=NEURON_RADIUS, stroke_color=neuron_stroke_color)
             neuron.set_stroke(width=neuron_stroke_width)
-            neuron.set_fill(color=BLACK, opacity=1.0)
+            if neuron_fills is None: 
+                neuron.set_fill(color='#000000', opacity=1.0)
+            else: 
+                neuron.set_fill(color=get_nueron_color(neuron_fills[1][i]), opacity=1.0)
             neuron.move_to(UP * ((HIDDEN_NEURONS//2 - i) * VERTICAL_SPACING))
             hidden_layer.add(neuron)
             
@@ -81,7 +106,10 @@ def get_mlp(w1,
         else:
             neuron = Circle(radius=NEURON_RADIUS, stroke_color=neuron_stroke_color)
             neuron.set_stroke(width=neuron_stroke_width)
-            neuron.set_fill(color=BLACK, opacity=1.0)
+            if neuron_fills is None: 
+                neuron.set_fill(color='#000000', opacity=1.0)
+            else: 
+                neuron.set_fill(color=get_nueron_color(neuron_fills[2][i]), opacity=1.0)
             neuron.move_to(RIGHT * LAYER_SPACING + UP * ((OUTPUT_NEURONS//2 - i) * VERTICAL_SPACING))
             output_layer.add(neuron)
             
@@ -119,11 +147,14 @@ class LlamaLearningSketchOne(InteractiveScene):
 
         w1 = np.random.randn(20, 24) 
         w2 = np.random.randn(24, 20)  
+        neuron_fills=[np.random.randn(20), np.random.randn(24), np.random.randn(20)]
 
         # net = Network([W1, W2])
         # self.add(net)
 
-        mlp=get_mlp(w1, w2)
+
+
+        mlp=get_mlp(w1, w2, neuron_fills)
         self.add(mlp)
         self.remove(mlp[2][1]); self.add(mlp[2][1]) #Ok seems like I'm just exploiting a bug, but this fixes layering. 
         self.remove(mlp[1][1]); self.add(mlp[1][1])
@@ -131,7 +162,32 @@ class LlamaLearningSketchOne(InteractiveScene):
         # Ok, making some progress here - I'm going to need to turn off some portion of the connections I think 
         # for it not be become a wall of fill. I think let's try just not drawing neuronss below some threshold?
 
+        # Alright so I have an MLP layer that I don't hate -> I think at this scale basically just turning off
+        # most of the connectiosn is the move. And I think that will look ok/fine. 
+        # And obviously I'll turn them on/off based on the actually values -> that will be nice!
+        # Ok, now there's a few different fun/intersting options I think. 
+        # Could have the circles and and attention patterns fill up left to right for forward prop -> that woudl probably be cool?
+        # Could be nice/interesting in monochrome brown opacities, or maybe viridis, we'll seee. 
+        # I of course could show forward passes along the synapses, we'll see. 
+        # Now backward passes I think are definitely along the synapses. 
+        # For extra credit I could use glowdots to actually traverse the gaps -> not sure if this is 
+        # visually helpful or not, wil keep that as a maybe option. 
+        # Ok one more visual option I think it to bring in the bubbles and/or line colors not all at once
+        # Could do min to max or randomly -> but having them kinda bubble in could be cool I think.
+        # Man i guess activation patterns are more like activations that weights - I guess that's whey thier called activation patterns lol
+        # For the monochrome colormap it might be cool to do that nice light tan as the brightest color. 
+        #hmm not sure I like viridis fills -> looks kinda meh.
+        # Ok monochrome is a little better, but I think the real problem with both is that you kidna want 
+        # most of them/the average to be close to black, ya know? 
+        # Can i just add an absolute value? Does that lose too much?
+        # Ok yeah the absolute value does help visually a bit i think. 
+        # Ok that will take some noodling, but I think it's not terrible. 
+        # I think it could be cool/interesting to have the activations in monochrom, and the grads in color. 
+
+        
         self.wait()
+
+
 
 
 
@@ -159,6 +215,100 @@ class LlamaLearningSketchOne(InteractiveScene):
 
 
 
+
+
+
+
+
+
+# def get_mlp(w1, 
+#             w2,
+#             line_weight=1.0, 
+#             line_opacity=0.5, 
+#             neuron_stroke_width=2.0, 
+#             neuron_stroke_color='#948979', 
+#             line_stroke_color='#948979', 
+#             connection_display_thresh=1.1):
+#     INPUT_NEURONS = w1.shape[0]
+#     HIDDEN_NEURONS = w1.shape[1]
+#     OUTPUT_NEURONS = w1.shape[0]
+#     NEURON_RADIUS = 0.08
+#     LAYER_SPACING = 0.25
+#     VERTICAL_SPACING = 0.25
+#     DOTS_SCALE=0.5
+    
+#     # Create layers
+#     input_layer = VGroup()
+#     hidden_layer = VGroup()
+#     output_layer = VGroup()
+#     dots = VGroup()
+    
+#     # Input layer
+#     for i in range(INPUT_NEURONS):
+#         if i == w1.shape[0]//2:  # Middle position for ellipsis
+#             dot = Tex("...").rotate(PI/2, OUT).scale(DOTS_SCALE).move_to(LEFT * LAYER_SPACING + UP * ((INPUT_NEURONS//2 - i) * VERTICAL_SPACING))
+#             dot.set_color(neuron_stroke_color)
+#             dots.add(dot)
+#         else:
+#             neuron = Circle(radius=NEURON_RADIUS, stroke_color=neuron_stroke_color)
+#             neuron.set_stroke(width=neuron_stroke_width)
+#             neuron.set_fill(color='#000000', opacity=1.0)
+#             neuron.move_to(LEFT * LAYER_SPACING + UP * ((INPUT_NEURONS//2 - i) * VERTICAL_SPACING))
+#             input_layer.add(neuron)
+            
+#     # Hidden layer
+#     for i in range(HIDDEN_NEURONS):
+#         if i == w1.shape[1]//2:  # Middle position for ellipsis
+#             dot = Tex("...").rotate(PI/2, OUT).scale(DOTS_SCALE).move_to(UP * ((HIDDEN_NEURONS//2 - i) * VERTICAL_SPACING))
+#             dot.set_color(neuron_stroke_color)
+#             dots.add(dot)
+#         else:
+#             neuron = Circle(radius=NEURON_RADIUS, stroke_color=neuron_stroke_color)
+#             neuron.set_stroke(width=neuron_stroke_width)
+#             neuron.set_fill(color='#000000', opacity=1.0)
+#             neuron.move_to(UP * ((HIDDEN_NEURONS//2 - i) * VERTICAL_SPACING))
+#             hidden_layer.add(neuron)
+            
+#     # Output layer
+#     for i in range(OUTPUT_NEURONS):
+#         if i == w1.shape[0]//2:  # Middle position for ellipsis
+#             dot = Tex("...").rotate(PI/2, OUT).scale(DOTS_SCALE).move_to(RIGHT * LAYER_SPACING + UP * ((OUTPUT_NEURONS//2 - i) * VERTICAL_SPACING))
+#             dot.set_color(neuron_stroke_color)
+#             dots.add(dot)
+#         else:
+#             neuron = Circle(radius=NEURON_RADIUS, stroke_color=neuron_stroke_color)
+#             neuron.set_stroke(width=neuron_stroke_width)
+#             neuron.set_fill(color='#000000', opacity=1.0)
+#             neuron.move_to(RIGHT * LAYER_SPACING + UP * ((OUTPUT_NEURONS//2 - i) * VERTICAL_SPACING))
+#             output_layer.add(neuron)
+            
+#     # Create connections with edge points
+#     connections = VGroup()
+    
+#     # Connect input to hidden layer
+#     for i, in_neuron in enumerate(input_layer):
+#         for j, hidden_neuron in enumerate(hidden_layer):
+#             if np.abs(w1[i, j])<connection_display_thresh: continue
+#             start_point, end_point = get_edge_points(in_neuron, hidden_neuron, NEURON_RADIUS)
+#             line = Line(start_point, end_point)
+#             line.set_stroke(opacity=np.clip(1.0*(np.abs(w1[i, j])-connection_display_thresh), 0, 1), width=line_weight)
+#             # print(np.clip(1.0*(np.abs(w1[i, j])-connection_display_thresh), 0, 1))
+#             line.set_color(line_stroke_color)
+#             connections.add(line)
+            
+#     # Connect hidden to output layer
+#     for i, hidden_neuron in enumerate(hidden_layer):
+#         for j, out_neuron in enumerate(output_layer):
+#             if np.abs(w2[i, j])<connection_display_thresh: continue
+#             start_point, end_point = get_edge_points(hidden_neuron, out_neuron, NEURON_RADIUS)
+#             line = Line(start_point, end_point) #, stroke_opacity=line_opacity, stroke_width=line_weight)
+#             line.set_stroke(opacity=np.clip(1.0*(np.abs(w2[i, j])-connection_display_thresh), 0, 1), width=line_weight)
+
+#             line.set_color(line_stroke_color)
+#             connections.add(line)
+
+                
+#     return VGroup(connections, input_layer, hidden_layer, output_layer, dots)
 
 
 

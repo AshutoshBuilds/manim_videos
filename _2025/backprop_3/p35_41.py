45c13005dabb5b3ebe5622a9e3afdfa73f42d9c2
Stephen Welch <stephen@Stephens-MBP.attlocal.net>
2025-08-02 13:02:06 -0400

    cant reproduce result?

--- _2025/backprop_3/p35_41.py
+++ _2025/backprop_3/p35_41.py
@@ -211,9 +211,13 @@ class p35_41(InteractiveScene):
         self.play(ShowCreation(outline, run_time=2))
         self.wait()
 
-        
+        #Ok ok ok I think i want to go overhead, may switch to top polygons, and for bonus points actually REplaceTransformt eh called
+        # out polygon and label! Than I can add netherlands belgium labels in illustrator. 
 
-
+        # Ok let me think ahead for a minute first though -> I want to show a single gradient descent step
+        # And I do think that showing all the actually gradient values on the network would be pretty cool!
+        # Anyway though, I need some example that actually makes sense lol -> Let me render out a loop of the first N steps 
+        # And see if I can make sense of what's going on -> I may need to combine steps too -> I think that would be ok!
 
 
 
@@ -236,8 +240,149 @@ class p35_41(InteractiveScene):
 
         
 
+class p36_loop_test(InteractiveScene):
+    def construct(self):
+
+        model = BaarleNet([3])
+        pickle_path='/Users/stephen/Stephencwelch Dropbox/welch_labs/backprop_3/hackin/3_starting_configs_4/training_data_seed_01_acc_0.8561.pkl'
+        with open(pickle_path, 'rb') as f:
+            p=pickle.load(f)
+
+        print(len(p['weights_history']), ' training steps loaded.')
+
+        viz_scales=[0.2, 0.2, 0.1]
+        num_neurons=[3, 3, 2]
+
+        self.frame.reorient(-1, 46, 0, (3.09, 0.56, -0.42), 7.25)
+
+        # train_step=2400
+        step_size=5
+        for train_step in np.arange(0, 50, step_size):
+            if train_step>0:
+                self.remove(group_12, group_13)
+                self.remove(group_21, group_22)
+                self.remove(group_31, group_32, lines)
+                self.remove(group_11)
+
+            w1=p['weights_history'][train_step]['model.0.weight'].numpy()
+            b1=p['weights_history'][train_step]['model.0.bias'].numpy()
+            w2=p['weights_history'][train_step]['model.2.weight'].numpy()
+            b2=p['weights_history'][train_step]['model.2.bias'].numpy()
+
+            with torch.no_grad():
+                model.model[0].weight.copy_(torch.from_numpy(w1))
+                model.model[0].bias.copy_(torch.from_numpy(b1))
+                model.model[2].weight.copy_(torch.from_numpy(w2))
+                model.model[2].bias.copy_(torch.from_numpy(b2))
 
 
+            #Precompute my surfaces, and polygons moving through network
+            surfaces=[]
+            surface_funcs=[]
+            for layer_idx in range(len(model.model)):
+                s=Group()
+                surface_funcs.append([])
+                for neuron_idx in range(num_neurons[layer_idx]):
+                    surface_func=partial(surface_func_from_model, model=model, layer_idx=layer_idx, neuron_idx=neuron_idx, viz_scale=viz_scales[layer_idx])
+                    bent_surface = ParametricSurface(surface_func, u_range=[-1, 1], v_range=[-1, 1], resolution=(64, 64))
+                    ts=TexturedSurface(bent_surface, graphics_dir+'/baarle_hertog_maps/baarle_hertog_maps-17.png')
+                    ts.set_shading(0,0,0).set_opacity(0.8)
+                    s.add(ts)
+                    surface_funcs[-1].append(surface_func)
+                surfaces.append(s)
+
+            #Move polygons through network
+            polygons={} #dict of all polygones as we go. 
+            polygons['-1.new_tiling']=[np.array([[-1., -1, 0], #First polygon is just input plane
+                                                [-1, 1, 0], 
+                                                [1, 1, 0], 
+                                                [1, -1, 0]])]
+
+            for layer_id in range(len(model.model)//2): #Move polygont through layers     
+                polygons[str(layer_id)+'.linear_out']=process_with_layers(model.model[:2*layer_id+1], polygons[str(layer_id-1)+'.new_tiling']) 
+                polygons[str(layer_id)+'.split_polygons_nested']=split_polygons_with_relu_simple(polygons[str(layer_id)+'.linear_out']) #Triple nested list so we can simplify merging process layer. 
+                polygons[str(layer_id)+'.split_polygons_nested_clipped'] = clip_polygons(polygons[str(layer_id)+'.split_polygons_nested'])
+                polygons[str(layer_id)+'.split_polygons_merged'] = merge_zero_regions(polygons[str(layer_id)+'.split_polygons_nested_clipped'])
+                polygons[str(layer_id)+'.new_tiling']=recompute_tiling_general(polygons[str(layer_id)+'.split_polygons_merged'])
+                print('Retiled plane into ', str(len(polygons[str(layer_id)+'.new_tiling'])), ' polygons.')
+
+            #Last linear layer & output
+            polygons[str(layer_id+1)+'.linear_out']=process_with_layers(model.model, polygons[str(layer_id)+'.new_tiling'])
+            intersection_lines, new_2d_tiling, upper_polytope, indicator = intersect_polytopes(*polygons[str(layer_id+1)+'.linear_out'])
+            my_indicator, my_top_polygons = compute_top_polytope(model, new_2d_tiling)
+
+
+            #Get first layer Relu Joints - 
+            joint_points_11 = get_relu_joint(w1[0,0], w1[0,1], b1[0], extent=1)
+            group_11=Group(surfaces[1][0])
+            if len(joint_points_11)>0:
+                joint_line_11=line_from_joint_points_1(joint_points_11).set_opacity(0.9)
+                group_11.add(joint_line_11)
+
+            group_12=Group(surfaces[1][1])
+            joint_points_12 = get_relu_joint(w1[1,0], w1[1,1], b1[1], extent=1)
+            if len(joint_points_12)>0:
+                joint_line_12=line_from_joint_points_1(joint_points_12).set_opacity(0.9)
+                group_12.add(joint_line_12)
+
+            group_13=Group(surfaces[1][2])
+            joint_points_13 = get_relu_joint(w1[2,0], w1[2,1], b1[2], extent=1)
+            if len(joint_points_13)>0:
+                joint_line_13=line_from_joint_points_1(joint_points_13).set_opacity(0.9)
+                group_13.add(joint_line_13)
+
+            group_11.shift([0, 0, 1.5])
+            group_13.shift([0, 0, -1.5])
+
+            polygons_21=manim_polygons_from_np_list(polygons['1.linear_out'][0], colors=colors, viz_scale=viz_scales[2], opacity=0.6)
+            polygons_21.shift([0, 0, 0.001]) #Move slightly above map
+
+            polygons_22=manim_polygons_from_np_list(polygons['1.linear_out'][1], colors=colors, viz_scale=viz_scales[2], opacity=0.6)
+            polygons_22.shift([0, 0, 0.001]) #Move slightly above map
+
+            group_21=Group(surfaces[2][0], polygons_21)
+            group_22=Group(surfaces[2][1], polygons_22)
+            group_21.shift([3.0, 0, 0.6])
+            group_22.shift([3.0, 0, -0.6])
+            group_21.set_opacity(0.5)
+            group_22.set_opacity(0.5)
+
+
+            #Ok now some plane intersction action in a third "panel"
+            group_31=group_21.copy()
+            group_31[1].set_color(BLUE)
+            group_31.shift([3, 0, -0.6])
+
+            group_32=group_22.copy()
+            group_32[1].set_color(YELLOW)
+            group_32.shift([3, 0, 0.6])
+
+            lines=VGroup()
+            for loop in intersection_lines: 
+                loop=loop*np.array([1, 1, viz_scales[2]])
+                line = VMobject()
+                line.set_points_as_corners(loop)
+                line.set_stroke(color='#FF00FF', width=5)
+                lines.add(line)
+            lines.shift([6, 0, 0])
+
+            #Make Baarle hertog maps a little mroe pronounced. 
+            group_31[0].set_opacity(0.9)
+            group_32[0].set_opacity(0.9)
+
+            # group_21.set_opacity(0.9)
+            # group_22.set_opacity(0.9)
+
+            # self.frame.reorient(-1, 46, 0, (3.09, 0.56, -0.42), 7.25)
+            self.add(group_12, group_13)
+            self.add(group_21, group_22)
+            self.add(group_31, group_32, lines)
+            self.add(group_11)
+            self.wait(0.1)
+
+
+
+            
 
         self.wait(20)
         self.embed()
